7dd7e13f85969f6c3bdb7bdfd6d0e30e37f46e75
mustafaltuntas <mustafairem2445@gmail.com>
2024-07-24 16:56:56 +0300

    read repository yapıldı

---
+++ Core/mustafaapi.Application/Interfaces/Repositories/IReadRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace mustafaapi.application.Interfaces.Repositories
+{
+	public class IReadRepository
+	{
+		public IReadRepository()
+		{
+		}
+	}
+}
+
---
+++ Infrastructure/mustafaapi.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace mustafaapi.Persistence.Repositories
+{
+	public class WriteRepository
+	{
+		public WriteRepository()
+		{
+		}
+	}
+}
+
